aa1d93ad2f6a6dcf816b87d91fac543d04b3eee5
diekmann <nobody>
2023-10-01 15:30:59 +0200

    Improve golang sample test cases
    Old output:
    
    $ go test -v ./Interface
    === RUN   TestSimple
    --- PASS: TestSimple (0.00s)
    PASS
    ok  	isabelle/exported/Interface	0.002s
    
    doesn't even look like it's doing something.
    
    New output at least explains a bit what we are testing:
    
    $ go test -v ./Interface
    === RUN   TestTreeFromList
    --- PASS: TestTreeFromList (0.00s)
    === RUN   TestJoinAndCheck
    --- PASS: TestJoinAndCheck (0.00s)
    === RUN   TestDelAndCheck
    --- PASS: TestDelAndCheck (0.00s)
    PASS
    ok  	isabelle/exported/Interface
    
    Also make tests a bit more idiomatic.

--- go_test/go/Interface/rbt_test.go
+++ go_test/go/Interface/rbt_test.go
@@ -1,27 +1,89 @@
 package rbt
 
 import (
-	"isabelle/exported/Bigint"
+	"reflect"
 	"testing"
+
+	"isabelle/exported/Bigint"
+	"isabelle/exported/RbtTest"
 )
 
-func TestSimple(t *testing.T) {
+type RBTBigInt = RbtTest.Prod[Bigint.Int, RbtTest.Color]
+
+func TestTreeFromList(t *testing.T) {
 	array := [5]Bigint.Int{Bigint.MkInt("1"), Bigint.MkInt("2"), Bigint.MkInt("3"), Bigint.MkInt("4"), Bigint.MkInt("5")}
 	elements := MkList(array[:])
 
-	t1, ok := TreeFromList(EqualInt, LinorderInt, elements)
+	got, ok := TreeFromList(EqualInt, LinorderInt, elements)
 	if !ok {
 		t.Fatal("new tree does not satisfy invariant")
 	}
-	_, ok = JoinAndCheck(LinorderInt, t1, t1)
+
+	want := RbtTest.Node[RBTBigInt]{
+		A: RbtTest.Node[RBTBigInt]{
+			A: RbtTest.Node[RBTBigInt]{
+				A: RbtTest.Leaf[RBTBigInt]{},
+				Aa: RBTBigInt{A: Bigint.MkInt("1"),
+					Aa: RbtTest.Black{},
+				},
+				Ab: RbtTest.Leaf[RBTBigInt]{},
+			},
+			Aa: RBTBigInt{
+				A:  Bigint.MkInt("2"),
+				Aa: RbtTest.Red{},
+			},
+			Ab: RbtTest.Node[RBTBigInt]{
+				A: RbtTest.Leaf[RBTBigInt]{},
+				Aa: RBTBigInt{A: Bigint.MkInt("3"),
+					Aa: RbtTest.Black{}},
+				Ab: RbtTest.Leaf[RBTBigInt]{},
+			},
+		},
+		Aa: RBTBigInt{
+			A:  Bigint.MkInt("4"),
+			Aa: RbtTest.Black{},
+		},
+		Ab: RbtTest.Node[RBTBigInt]{
+			A: RbtTest.Leaf[RBTBigInt]{},
+			Aa: RBTBigInt{
+				A:  Bigint.MkInt("5"),
+				Aa: RbtTest.Black{},
+			},
+			Ab: RbtTest.Leaf[RBTBigInt]{},
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got tree does not equal want tree")
+	}
+}
+
+func TestJoinAndCheck(t *testing.T) {
+	list := MkList([]Bigint.Int{Bigint.MkInt("1"), Bigint.MkInt("2"), Bigint.MkInt("3"), Bigint.MkInt("4"), Bigint.MkInt("5")})
+
+	tree, ok := TreeFromList(EqualInt, LinorderInt, list)
+	if !ok {
+		t.Fatal("new tree does not satisfy invariant")
+	}
+	_, ok = JoinAndCheck(LinorderInt, tree, tree)
 	if !ok {
 		t.Fatal("joining trees violated invariant")
 	}
-	t2, ok := DelAndCheck(EqualInt, LinorderInt, elements, t1)
+}
+
+func TestDelAndCheck(t *testing.T) {
+	list := MkList([]Bigint.Int{Bigint.MkInt("1"), Bigint.MkInt("2"), Bigint.MkInt("3"), Bigint.MkInt("4"), Bigint.MkInt("5")})
+
+	tree, ok := TreeFromList(EqualInt, LinorderInt, list)
+	if !ok {
+		t.Fatal("new tree does not satisfy invariant")
+	}
+	got, ok := DelAndCheck(EqualInt, LinorderInt, list, tree)
 	if !ok {
 		t.Fatal("deletion violated invariant")
 	}
-	ok = TreesEqual(EqualInt, t2, EmptyTree[Bigint.Int]())
+	want := EmptyTree[Bigint.Int]()
+	ok = TreesEqual(EqualInt, got, want)
 	if !ok {
 		t.Fatal("tree not empty after deletion")
 	}
